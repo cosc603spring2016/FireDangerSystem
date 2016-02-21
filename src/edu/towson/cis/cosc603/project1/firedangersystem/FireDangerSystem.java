9c906981dc42626a44ee9d37dc0ef579f600598e
cosc603spring2016 <cosc603spring2016@gmail.com>
2016-02-21 15:24:36 -0500

    COSC 603 Project 1
    Reengineering of a Fortran 77 fire danger system

---
+++ src/edu/towson/cis/cosc603/project1/firedangersystem/FireDangerSystem.java
@@ -0,0 +1,10 @@
+package edu.towson.cis.cosc603.project1.firedangersystem;
+
+public class FireDangerSystem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
